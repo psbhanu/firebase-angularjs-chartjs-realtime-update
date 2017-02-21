cf82e1ab1553892aa4d9833749a704e3e4606f63
psbhanu <psbhanu@outlook.com>
2017-02-21 14:07:02 +0530

    First Commit after POC get ready

---
+++ client/app.js
@@ -0,0 +1,124 @@
+(function () {
+	'use strict';
+	var app = angular.module('iApp', ['chart.js', 'ui.bootstrap']);
+	
+	app.controller('ChartCtrl', ['$scope', '$interval', function ($scope, $interval) {
+		var maximum = 100 || document.getElementById('container').clientWidth / 2 || 300;
+		//$scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+		$scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+		$scope.data = [[]];
+		$scope.labels = [];
+		$scope.options = {
+			animation: {
+				duration: 0
+			},
+			elements: {
+				line: {
+					borderWidth: 0.5
+				},
+				point: {
+					radius: 2
+				}
+			},
+			legend: {
+				display: false
+			},
+			scales: {
+				xAxes: [{
+					display: true
+				}],
+				yAxes: [{
+					display: true
+				}],
+				gridLines: {
+					display: true
+				}
+			},
+			tooltips: {
+				enabled: true
+			}
+		};
+		
+		// Update the dataset at 25FPS for a smoothly-animating chart
+		//$interval(function () {
+		//getLiveChartData();
+		//}, 800);
+	
+		//$interval(function () {}, 0);
+	
+		//$scope.labels = [];
+		function getLiveChartData () {
+			if ($scope.data[0].length) {
+				$scope.labels = $scope.labels.slice(1);
+				$scope.data[0] = $scope.data[0].slice(1);
+			}
+			
+			while ($scope.data[0].length < maximum) {
+				$scope.labels.push('');
+				$scope.data[0].push(getRandomValue($scope.data[0]));
+			}
+		}
+	
+		$scope.updateLiveChartData = function (data) {
+			if ($scope.data[0].length) {
+				$scope.labels = $scope.labels.slice(1);
+				$scope.data[0] = $scope.data[0].slice(1);
+			}
+			
+			while ($scope.data[0].length < maximum) {
+				$scope.labels.push('');
+				$scope.data[0].push(data);
+			}
+			$scope.$apply();
+		}
+	
+		var iFirebaseClient = new firebaseClient(function(snapshot){
+			console.log(snapshot.val());
+			$scope.updateLiveChartData (snapshot.val().point);
+		});
+	
+	}]);
+	
+	var firebaseClient = function(onValue){
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyA42Yct7VLWgnnEEusWuY5tPlFTqFj2F5A",
+			authDomain: "fir-realtime-e9a30.firebaseapp.com",
+			databaseURL: "https://fir-realtime-e9a30.firebaseio.com",
+			storageBucket: "fir-realtime-e9a30.appspot.com",
+			messagingSenderId: "516180830611"
+		};
+		
+		//const DEFAULT_PATH = '/sbts';
+		const DEFAULT_PATH = '/points';
+		
+		var mainFirebaseApp = firebase.initializeApp(config);
+		console.log(mainFirebaseApp);
+		var ref = mainFirebaseApp.database().ref(DEFAULT_PATH); //.getKey('vehicle_unique_id');
+		
+		// Attach an asynchronous callback to read the data at our posts reference
+		/*
+		ref.on("value", function(snapshot) {
+			console.log(snapshot.val());
+			}, function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+		});
+		*/
+		
+		this.onValue = onValue || function(snapshot) {
+			console.log(snapshot.val());
+		};
+		
+		this.onError = function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+		};
+		
+		ref.on("value", this.onValue, this.onError);
+	};
+	
+	function getRandomValue (data) {
+		var l = data.length, previous = l ? data[l - 1] : 50;
+		var y = previous + Math.random() * 10 - 5;
+		return y < 0 ? 0 : y > 100 ? 100 : y;
+	}
+	})();		
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require 'vendor/autoload.php';
+
+//$firebase = Firebase::fromServiceAccount(__DIR__.'/fir-realtime-e9a30-firebase-adminsdk-ba300-1e97c922b6.json');
+$dateTime = new DateTime();
+//server/saving-data/fireblog/posts
+$databaseUri 	= 'https://fir-realtime-e9a30.firebaseio.com';
+$secret 		= 'TMNoYQdPXanf3lRRsqNksPwgSzzlA1EEzcdJUFn3';
+$firebase = Firebase::fromDatabaseUriAndSecret($databaseUri, $secret);
+
+$database = $firebase->getDatabase();
+
+//$newPost = $database->getReference('/points')->push([ 'point' => 100 ]);
+
+while(true){
+	$newPost = $database->getReference('/points')->set([
+	   'point' => rand(10,1000)
+	]);
+	sleep(1);
+}
+
+
+//$db->getReference('config/website/name')->set('New name');
+
+//$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+print_r($newPost);
+
+//$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+//$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+
+//$newPost->getChild('title')->set('Changed post title');
+//$newPost->getValue(); // Fetches the data from the realtime database
+//$newPost->remove();
